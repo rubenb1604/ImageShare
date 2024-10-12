6ecc8491bf125479646d7d2b0a2f2c8d000260b0
rubenb1604 <rubenbertram@gmx.de>
2024-10-12 15:38:16 +0200

    copy try

--- components/TitleField.tsx
+++ components/TitleField.tsx
@@ -128,12 +128,10 @@ const TitleField: React.FC<FieldProps> = ({ link }) => {
           <h1 className="font-semibold text-2xl text-center">Dein Link:</h1>
           <a
             href={`https://image-share-mu.vercel.app/view/${encryptId(linkId || "")}`}
-            className="text-center text-blue-700 underline max-w-full"
+            className="text-center text-blue-700 underline max-w-full font-sans text-xs"
             
           >
-
-            
-            https://image-share-mu.vercel.app/view/{encryptId(linkId || "")}
+            Click me
           </a>
         </div>
       )}
